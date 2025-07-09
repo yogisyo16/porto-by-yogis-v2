969a790149cbd3114b3c43b31ad4a694546ed974
yogis <yogisyo16@gmail.com>
2025-07-09 15:34:30 +0700

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import PtHeader from "@/components/PtHeader";
 
 export default function Home() {
-    return<></>
+    return
+    <>
+        <PtHeader />
+    </>
 }
--- components/PtHeader.tsx
+++ components/PtHeader.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-interface PtHeaderProps {
+// interface PtHeaderProps {
 
-}
+// }
 
 export default function PtHeader() {
     return 
     <>
-        
+        <div className="bg-amber-300 w-full">
+            <h1>Test</h1>
+        </div>
     </>;
 }
