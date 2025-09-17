ac02bba79ce50bcf23022f9d4f2f75e3c9a15453
yogisyo16 <yogisyo16@gmail.com>
2025-09-17 21:08:51 +0700

    Update: clean code

--- components/AboutPage/PyAbout.tsx
+++ components/AboutPage/PyAbout.tsx
@@ -3,16 +3,9 @@ import React from "react";
 export default function PyAbout() {
     return (
         <>
-            <section className="flex flex-col md:justify-center md:w-full md:flex-grow md:pt-[40px]">
-                <div className="flex flex-row md:justify-center"> {/* Also added justify-center here to center your text */}
-                    <div className="flex md:flex-col md:px-[10px]">
-                        <h1 className="text-6xl">Hello there</h1>
-                        <h1 className="text-6xl">Yogiswara here</h1>
-                    </div>
-                    <div className="flex md:flex-col md:px-[10px]">
-                        <h1 className="text-6xl">I am</h1>
-                        <h2>Software Engineer</h2>
-                    </div>
+            <section className="flex flex-col md:justify-center md:w-full md:flex-grow pt-[20px] md:pt-[40px]">
+                <div className="flex flex-row items-center md:items-center justify-center md:justify-center"> {/* Also added justify-center here to center your text */}
+                    <h2 className="text-2xl font-bold">About</h2>
                 </div>
             </section>
         </>
